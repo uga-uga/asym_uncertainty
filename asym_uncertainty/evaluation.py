ac3673d6bb50e08aa918980ae56fa2094df8166b
TB-IKP <tbeck@ikp.tu-darmstadt.de>
2019-10-24 23:22:36 +0200

    Included KDE in evaluation.py

--- asym_uncertainty/evaluation.py
+++ asym_uncertainty/evaluation.py
@@ -16,24 +16,39 @@ values from a distribution"""
 #    You should have received a copy of the GNU General Public License
 #    along with asym_uncertainty.  If not, see <http://www.gnu.org/licenses/>.
 
-from numpy import argmax, extract, histogram
+from numpy import argmax, extract, histogram, median
+from scipy.optimize import minimize
+from scipy.stats import gaussian_kde
 
 from .mc_statistics import cdf, shortest_coverage
 
-def evaluate(rand_result, force_inside_shortest_coverage=True):
+def evaluate(rand_result, force_inside_shortest_coverage=True, use_kde=True):
     """Implementation of Unc.eval()"""
 
     s_cov = shortest_coverage(cdf(rand_result))
     if force_inside_shortest_coverage:
-        hist, bins = histogram(
-            extract((rand_result >= s_cov[0])*(rand_result <= s_cov[1]), rand_result),
-            bins="sqrt")
+    	
+    	if use_kde:
+    	    data_inside = extract((rand_result >= s_cov[0])*(rand_result <= s_cov[1]), rand_result)
+    	    
+    	    plain_kde = gaussian_kde(data_inside)
+    	    kde = lambda x: (-1)*plain_kde.evaluate(x)
+    	    most_probable = minimize(kde,x0=median(data_inside),
+    	        method='TNC',tol=1e-5,bounds=((min(data_inside),max(data_inside)),)).x[0]
+
+    	else:
+            hist, bins = histogram(
+                extract((rand_result >= s_cov[0])*(rand_result <= s_cov[1]), rand_result),
+                bins="sqrt")
+
+            most_probable = bins[argmax(hist)]+0.5*(bins[1]-bins[0])
+
 # In the context of asym_uncertainty, this case will never occur.
 # However, it was decided to leave the 'else' statement here as a reminder that
 # the force_inside_shortest_coverage option is set.
 #    else:
 #        hist, bins = histogram(rand_result, bins="sqrt")
 
-    most_probable = bins[argmax(hist)]+0.5*(bins[1]-bins[0])
+    
 
     return ([most_probable, most_probable - s_cov[0], s_cov[1] - most_probable], rand_result)
