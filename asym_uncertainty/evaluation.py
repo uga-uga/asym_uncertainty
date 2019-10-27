51d24b44f4807c4e365cd4c26c94a342b0194e26
TB-IKP <tbeck@ikp.tu-darmstadt.de>
2019-10-27 10:47:13 +0100

    Update mode with KDE
    - switched from minimize to minimize_scalar
    - included warning if minimization of KDE fails
    - added fallback mechanism: result from discretization is used if minimization fails

--- asym_uncertainty/evaluation.py
+++ asym_uncertainty/evaluation.py
@@ -16,8 +16,10 @@ values from a distribution"""
 #    You should have received a copy of the GNU General Public License
 #    along with asym_uncertainty.  If not, see <http://www.gnu.org/licenses/>.
 
+import warnings
+
 from numpy import argmax, extract, histogram, median
-from scipy.optimize import minimize
+from scipy.optimize import minimize, minimize_scalar
 from scipy.stats import gaussian_kde
 
 from .mc_statistics import cdf, shortest_coverage
@@ -26,22 +28,25 @@ def evaluate(rand_result, force_inside_shortest_coverage=True, use_kde=True):
     """Implementation of Unc.eval()"""
 
     s_cov = shortest_coverage(cdf(rand_result))
+    
     if force_inside_shortest_coverage:
-    	
-    	if use_kde:
-    	    data_inside = extract((rand_result >= s_cov[0])*(rand_result <= s_cov[1]), rand_result)
-    	    
-    	    plain_kde = gaussian_kde(data_inside)
-    	    kde = lambda x: (-1)*plain_kde.evaluate(x)
-    	    most_probable = minimize(kde,x0=median(data_inside),
-    	        method='TNC',tol=1e-5,bounds=((min(data_inside),max(data_inside)),)).x[0]
+        
+        data_inside     = extract((rand_result >= s_cov[0])*(rand_result <= s_cov[1]), rand_result)    	
+        hist, bins      = histogram(data_inside,bins="sqrt")
+        most_probable   = bins[argmax(hist)]+0.5*(bins[1]-bins[0])
 
-    	else:
-            hist, bins = histogram(
-                extract((rand_result >= s_cov[0])*(rand_result <= s_cov[1]), rand_result),
-                bins="sqrt")
+        if use_kde:
 
-            most_probable = bins[argmax(hist)]+0.5*(bins[1]-bins[0])
+            plain_kde   = gaussian_kde(data_inside)
+            inv_kde     = lambda x: (-1)*plain_kde.evaluate(x)
+            min_kde     = minimize_scalar(inv_kde,bounds=((s_cov[0],s_cov[1]),))
+
+            if min_kde.success:
+                most_probable  = min_kde.x[0]   
+            else:
+                warnings.warn('Minimization of KDE failed due to %s. Use histogram-generated mode instead.'% min_kde.message,
+                    UserWarning)
+            
 
 # In the context of asym_uncertainty, this case will never occur.
 # However, it was decided to leave the 'else' statement here as a reminder that
